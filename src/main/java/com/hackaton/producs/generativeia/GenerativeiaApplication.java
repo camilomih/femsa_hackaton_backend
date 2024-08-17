d6d8f436bb3d96cb15720f1bd358f26c8a182a98
Camilo Valencia Galindo <juancamilovalenciagalindo@192.168.1.14>
2024-08-17 14:19:44 -0500

    Configuracion para crear la db y configurarla en el proyecto

---
+++ src/main/java/com/hackaton/producs/generativeia/GenerativeiaApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.producs.generativeia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerativeiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerativeiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/producs/generativeia/GenerativeiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.producs.generativeia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerativeiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
